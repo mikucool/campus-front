b02903fb95de24ccf54d8e1fff96032ebd624c59
hzz_global <72819839+mikucool@users.noreply.github.com>
2022-03-26 16:40:36 +0800

    init project，created billborad.

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// Buefy
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+// Element UI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+
+Vue.use(Buefy)
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <!--公告栏-->
+  <div>
+    <div class="box">{{billboard}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      // 展示在公告栏的数据
+      billboard:"前端之王"
+    }
+  }
+}
+</script>
