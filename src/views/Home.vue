a240a39d4eb00bac2464315c89fbbdac7b6ddd3d
hzz_global <72819839+mikucool@users.noreply.github.com>
2022-03-27 20:40:39 +0800

    完善公告板，解决跨域，连接后端 20220327

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,34 @@
 <template>
-  <!--公告栏-->
   <div>
-    <div class="box">{{billboard}}</div>
+    <!--公告栏-->
+    <div class="box">{{billboard.description}}</div>
   </div>
 </template>
 
 <script>
+import {getBillboard} from '@/api/billboard'
 
 export default {
   name: 'Home',
   data() {
     return {
       // 展示在公告栏的数据
-      billboard:"前端之王"
+      billboard: {
+        description: ''
+      }
     }
+  },
+  created() {
+    this.fetchBillboard()
+  },
+  methods : {
+    // 异步方法，请求获取 billboard 的方法
+    async fetchBillboard() {
+      getBillboard().then((value) => {
+        const { data } = value  // value：返回的 billboard 对象
+        this.billboard = data
+      })
+    },
   }
 }
 </script>
