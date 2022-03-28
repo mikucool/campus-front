30c84f4a80ef71bf6d5da8e9202dd3fb33195a4b
hzz_global <72819839+mikucool@users.noreply.github.com>
2022-03-28 21:18:36 +0800

    完善推广模块

--- src/views/card/Promotion.vue
+++ src/views/card/Promotion.vue
@@ -4,25 +4,34 @@
       <span>🥂 推广</span>
     </div>
     <div>
-      body
+      <p v-for="(item, index) in list" :key="index" class="block">
+        <!-- _blank:使用新的页面打开 -->
+        <a :href="item.link" target="_blank">{{ item.title }}</a>
+      </p>
     </div>
   </el-card>
 </template>
 
 <script>
+import {getList} from '@/api/promotion'
 
 export default {
   name: 'Promotion',
   data() {
     return {
-
+      list: []
     }
   },
   created() {
-    
+    this.fetchList();
   },
   methods: {
-    
+    fetchList() {
+      getList().then(response => {
+        const {data} = response;
+        this.list = data;
+      })
+    }
   }
 }
 </script>
